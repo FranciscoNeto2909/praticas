b08c66a7ff12ad0d7ba203e78dd44d20375f9845
Francisco Neto <wantedshark34@gmail.com>
2022-02-21 16:14:49 -0300

    first commit in this branch

--- src/App.js
+++ src/App.js
@@ -1,43 +1,37 @@
 import React, { useState } from "react";
-import Button from "./components/Button";
-import List from "./components/listas/List";
-
+import "./App.css"
 export default function App() {
-  const list = [
-    {
-      name: "João",
-      age: 19
-    },
-    {
-      name: "Maria",
-      age: 12
-    },
-    {
-      name: "Antôno",
-      age: 29
-    }
-    , {
-      name: "Carol",
-      age: 23
-    },
-    {
-      name: "Neto",
-      age: 20
-    }]
 
-  const [showList, setShowList] = useState(false)
-
-  const handleShowList = () => {
-    setShowList(!showList)
+  const link = "https://api.b7web.com.br/cinema/"
+  const [movies,setMovies] = useState([])
+  function loadMovies(e) {
+    e.preventDefault()
+    fetch(link)
+      .then(res => res.json())
+      .then(res => setMovies(res))
   }
-
   return (
     <div>
-      <h1>Praticas do curso b7 web</h1>
-      <h2>Lista de participantes</h2>
-      <Button eventClick={handleShowList} text={showList ? "Esconder lista" : "Mostrar lista"} />
-      <List list={list} showList={showList} />
+      <h1 className="text-center">Praticas do curso b7 web</h1>
+      <h2>Requisições:</h2>
+      <p>Requisições são formas de solicitar o acesso aos dados de um link, api ou banco de dados. existem varios tipos de requisições mas os mais famosos são: GET, POST, PUT e DELETE.</p>
+
+      <h3>GET:</h3>
+      <p>A requisição do tipo GET apenas retorna os dados.</p>
+      <p>O exemplo a seguir acessa os filmes de uma api e os retorna para serem manipulados pelo programador</p>
+      <div>
+        <p>Total de filmes: {movies.length}</p>
+        <button className="btn btn-primary" onClick={loadMovies}>Carregar filmes</button>
+        <div className="row">
+          {movies.map((movie,index)=>(
+            <div className="card col-5 m-2" key={index}>
+                <img src={movie.avatar} alt="movie" className="rounded card-img my-2"/>
+                <h4 className="card-title text-center">{movie.titulo}</h4>
+            </div>
+          ))}
+        </div>
+      </div>
+
     </div>
   );
-  //Observação: Ao renderizar listas o valor sera retornado automaticamente se passado em um parenteses ao invez de chaves.
 }
