cfb6f2f677b338ee2499b790ef03bea26af2a752
Francisco Neto <wantedshark34@gmail.com>
2022-03-02 17:37:03 -0300

    add project with hook useReducer

--- src/components/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-export default function Button({eventClick, text}) {
-
-    return(
-    <button className="btn bg-primary text-white mx-2" onClick={eventClick}>{text}</button>
-    )
-}
